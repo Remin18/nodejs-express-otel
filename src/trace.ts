3fdf708bb90f09d2d629fd4e9dd0364bd61d035d
t.egawa <egawa.takeki@classmethod.jp>
2025-01-29 17:54:43 +0900

    コンソールにログを吐き出すところまで

--- src/trace.ts
+++ src/trace.ts
@@ -1,17 +1,27 @@
-import { NodeSDK } from '@opentelemetry/sdk-node';
-import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
-import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
-import { Resource } from '@opentelemetry/resources';
-import { ATTR_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
+import { NodeSDK } from "@opentelemetry/sdk-node";
+import { ConsoleSpanExporter } from "@opentelemetry/sdk-trace-base";
+import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
+import {
+  PeriodicExportingMetricReader,
+  ConsoleMetricExporter,
+} from "@opentelemetry/sdk-metrics";
+import { HttpInstrumentation } from "@opentelemetry/instrumentation-http";
+import { ExpressInstrumentation } from "@opentelemetry/instrumentation-express";
+// import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-grpc";
+// import { OTLPMetricExporter } from "@opentelemetry/exporter-metrics-otlp-grpc";
 
+// OpenTelemetry の SDK を初期化する
 const sdk = new NodeSDK({
-  resource: new Resource({
-    [ATTR_SERVICE_NAME]: 'todo-service',
+  traceExporter: new ConsoleSpanExporter(),
+  metricReader: new PeriodicExportingMetricReader({
+    exporter: new ConsoleMetricExporter(),
   }),
-  traceExporter: new OTLPTraceExporter({
-    url: process.env.OTEL_EXPORTER_OTLP_ENDPOINT || 'http://localhost:4317',
-  }),
-  instrumentations: [getNodeAutoInstrumentations()],
+  instrumentations: [
+    getNodeAutoInstrumentations(),
+    new HttpInstrumentation(),
+    new ExpressInstrumentation(),
+  ],
 });
 
+// SDK を起動することで自動で計装が開始される
 sdk.start();
