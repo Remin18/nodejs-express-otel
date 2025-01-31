d9b48a9e86e6ef3d973c955f845b509106a68354
t.egawa <egawa.takeki@classmethod.jp>
2025-01-31 14:08:35 +0900

    fix: optl settings

--- src/trace.ts
+++ src/trace.ts
@@ -1,20 +1,21 @@
 import { NodeSDK } from "@opentelemetry/sdk-node";
-import { ConsoleSpanExporter } from "@opentelemetry/sdk-trace-base";
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
 import {
   PeriodicExportingMetricReader,
-  ConsoleMetricExporter,
 } from "@opentelemetry/sdk-metrics";
 import { HttpInstrumentation } from "@opentelemetry/instrumentation-http";
 import { ExpressInstrumentation } from "@opentelemetry/instrumentation-express";
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-grpc";
 import { OTLPMetricExporter } from "@opentelemetry/exporter-metrics-otlp-grpc";
+import { ATTR_SERVICE_NAME } from "@opentelemetry/semantic-conventions";
+import { Resource } from "@opentelemetry/resources";
 
 const sdk = new NodeSDK({
-  // traceExporter: new ConsoleSpanExporter(),
+  resource: new Resource({
+    [ATTR_SERVICE_NAME]: process.env.SERVICE_NAME,
+  }),
   traceExporter: new OTLPTraceExporter({ url: process.env.OTEL_EXPORTER_OTLP_ENDPOINT }),
   metricReader: new PeriodicExportingMetricReader({
-    // exporter: new ConsoleMetricExporter(),
     exporter: new OTLPMetricExporter({ url: process.env.OTEL_EXPORTER_OTLP_ENDPOINT }),
   }),
   instrumentations: [
