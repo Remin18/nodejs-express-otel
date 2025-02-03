c8721f26727cc80625ceafe42d1953ad96d732d2
t.egawa <egawa.takeki@classmethod.jp>
2025-02-03 14:01:11 +0900

    fix

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,8 +1,6 @@
 import { NodeSDK } from "@opentelemetry/sdk-node";
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
 import { PeriodicExportingMetricReader } from "@opentelemetry/sdk-metrics";
-import { HttpInstrumentation } from "@opentelemetry/instrumentation-http";
-import { ExpressInstrumentation } from "@opentelemetry/instrumentation-express";
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-grpc";
 import { OTLPMetricExporter } from "@opentelemetry/exporter-metrics-otlp-grpc";
 import { ATTR_SERVICE_NAME } from "@opentelemetry/semantic-conventions";
@@ -17,9 +15,10 @@ const sdk = new NodeSDK({
     exporter: new OTLPMetricExporter({ url: process.env.OTEL_EXPORTER_OTLP_ENDPOINT }),
   }),
   instrumentations: [
-    getNodeAutoInstrumentations(),
-    new HttpInstrumentation(),
-    new ExpressInstrumentation(),
+    getNodeAutoInstrumentations({
+      "@opentelemetry/instrumentation-fs": { enabled: false },
+      "@opentelemetry/instrumentation-express": { enabled: true },
+    }),
   ],
 });
 
